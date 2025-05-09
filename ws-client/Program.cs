c5469a41a7b47c3d40dd9210ca63d7d84de60879
habibuoy <habibiemuhammad90@gmail.com>
2025-05-09 16:21:54 +0700

    Initial commit
    - Server: app using ASP.NET Core
    - Client: a console app

---
+++ ws-client/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Main:
+Console.Clear();
+Console.WriteLine("Hello, World! Type exit to quit.");
+Console.Write("Input: ");
+
+var line = Console.ReadLine();
+if (line == "exit")
+{
+    return;
+}
+else
+{
+    goto Main;
+}
