cc8cbf1dc2a955578a1714685d2234ae59fb2a66
habibuoy <habibiemuhammad90@gmail.com>
2025-05-10 15:31:37 +0700

    Add simple websocket client

--- ws-client/Program.cs
+++ ws-client/Program.cs
@@ -1,15 +1,65 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Main:
-Console.Clear();
-Console.WriteLine("Hello, World! Type exit to quit.");
-Console.Write("Input: ");
 
-var line = Console.ReadLine();
-if (line == "exit")
+using System.Net.WebSockets;
+using System.Text;
+
+const string WebsocketEndpoint = "wss://localhost:7000/ws";
+
+ClientWebSocket websocket = null;
+
+async Task RunWebsocket()
 {
-    return;
+    if (websocket == null)
+    {
+        var answer = string.Empty;
+
+        while (answer != "Y" && answer != "y")
+        {
+            Console.Write("Begin websocket? (Y/y): ");
+            answer = Console.ReadLine();
+        }
+
+        websocket = new ClientWebSocket();
+    }
+    else
+    {
+        await websocket.ConnectAsync(new Uri(WebsocketEndpoint), CancellationToken.None);
+
+        var buffer = new byte[1024];
+        var message = Encoding.UTF8.GetBytes("Ping");
+
+        while (true)
+        {
+            WebSocketReceiveResult receiveResult = null;
+
+            await websocket.SendAsync(message, WebSocketMessageType.Text, true, CancellationToken.None);
+            receiveResult = await websocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+            if (receiveResult != null)
+            {
+                if (!receiveResult.CloseStatus.HasValue)
+                {
+                    if (receiveResult.MessageType == WebSocketMessageType.Text)
+                    {
+                        var receivedMessage = Encoding.UTF8.GetString(buffer);
+                        Console.WriteLine($"Message from server: {receivedMessage}");
+                    }
+                }
+                else
+                {
+                    await websocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Normal", CancellationToken.None);
+                    break;
+                }
+            }
+
+            await Task.Delay(1000);
+        }
+        
+        websocket = null!;
+    }
 }
-else
+
+while (true)
 {
-    goto Main;
+    await RunWebsocket();
 }
